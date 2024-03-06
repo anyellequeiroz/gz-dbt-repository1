1407e1f2f9cb4b38857bff9675bdd0e43ba1d353
Anyelle Queiroz <anyellequeiroz13@gmail.com>
2024-03-06 08:09:17 +0000

    oper

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,7 @@
 SELECT
   o.orders_id
   ,o.date_date
-  ,ROUND(s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
+  ,ROUND(s.shipping_fee - (s.logcost + CAST(s.ship_cost AS FLOAT64)), 2) AS operational_margin 
   ,o.quantity
   ,o.revenue
   ,o.purchase_cost
@@ -13,3 +13,4 @@ FROM {{ref("int_orders_margin")}} o
 LEFT JOIN {{ref("stg_raw__ship")}} s 
   USING(orders_id)
 ORDER BY orders_id desc
+
